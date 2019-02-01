25856091cbf5a73851312b433d957107a61dc452
Endy Ranaudo <endyranaudo@gmail.com>
2019-02-01 12:38:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(arr, element){
-  
+  arr.unshift(element);
 }
 
 function destructivelyAddElementToBeginningOfArray(){
